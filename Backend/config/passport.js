4c0fca48683466cf0aa6f9d30a4fb0d534c6e459
DJ <social.deej@gmail.com>
2020-07-06 02:54:16 +0530

    Matching the User Auth

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -7,8 +7,14 @@ const passport = require('passport')
 
 module.exports = function (passport){
     passport.use(
-        new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
+        new LocalStrategy({ usernameField: 'email'}, async (email, password, done) => {
             
+            const user = await User.findOne({email: email })
+            if (!user)
+                return done (null, false, {message: 'Email is not Registered '})
+            
+                
+
         })
     )
 }
