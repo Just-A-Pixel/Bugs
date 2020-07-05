a2c2ddb9e41a81e04841be314d605622e5bad8d8
DJ <social.deej@gmail.com>
2020-07-06 01:56:54 +0530

    Configuring User

--- Backend/routers/users.js
+++ Backend/routers/users.js
@@ -2,6 +2,10 @@
 
 const express = require('express')
 const router = express.Router();
+const User = require('../models/User')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+
 
 // Login 
 router.get('/login', (req, res) => {
@@ -12,11 +16,23 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
 })
 
-router.post('/register', (req, res) => {
-    const {name, email, password, password2} = req.body ;
+router.post('/register', async (req, res) => {
+    // {name, email, password, password2} = req.body ;
     
-    if (password != password2)
-        
+    const email = req.body.email
+    const checkUser = await User.findOne({email})
+
+    if (checkUser)
+        throw new Error('An User Already Exists !!')
+    
+    const user = new User(req.body)
+    
+    if (user.password != user.password2)
+        throw new Error('Passwords Should be same !!')
+    
+    await user.save(); 
+
+    res.send(user);
 })
 
 module.exports = router ;
