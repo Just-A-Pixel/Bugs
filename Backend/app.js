18684472c1869b7171251d08ca10c344f8cc656a
DJ <social.deej@gmail.com>
2020-07-06 03:09:42 +0530

    Completed Passport Auth For User Signed Email

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const passport = require('passport')
 
 require('./database/mongoose')
+require('./config/passport')(passport)
 const app = express()
+
 
 const PORT = process.env.PORT || 3000 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 
 app.use('/', require('./routers/index'))
--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -9,12 +9,43 @@ module.exports = function (passport){
     passport.use(
         new LocalStrategy({ usernameField: 'email'}, async (email, password, done) => {
             
-            const user = await User.findOne({email: email })
-            if (!user)
-                return done (null, false, {message: 'Email is not Registered '})
-            
+            try {
                 
+                const user = await User.findOne({email: email })
+                
+                if (!user)
+                    return done (null, false, {message: 'Email is not Registered '})
+                
+                
+                bcrypt.compare(password, user.password, (error, isMatch) => {
+                    if (error){
+                        res.send('Error : Password Doesn\'t Matches ')
+                        throw error 
+                    }
+    
+                    if (isMatch){
+                        return done (null, user)
+                    } else {
+                        return done (null, false, {message: 'Password Incorrect!!'})
+                    }
+    
+                })
+
+            }catch(error){
+                res.send('Error Has Been Encountered ' + error);
+            }
 
         })
     )
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+      
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function (err, user) {
+          done(err, user);
+        });
+    });
+      
 }
