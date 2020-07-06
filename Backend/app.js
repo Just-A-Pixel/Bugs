e35083b6ec453bf8a02f0c544c6b9413fca513d5
DJ <social.deej@gmail.com>
2020-07-06 22:55:22 +0530

    Added Google Auth2.0

--- Backend/app.js
+++ Backend/app.js
@@ -18,16 +18,18 @@ const PORT = process.env.PORT || 3000
 app.use(flash());
 
 app.use(
-    session({
-      secret: process.env.SECRET,
-      resave: true,
-      saveUninitialized: true,
-      store: new MongoStore({
-        url: process.env.MONGOURI,
-        collection: 'sessions'
-      })
-    }) 
-  );
+  session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({
+      url: process.env.MONGOURI,
+      collection: 'sessions'
+    })
+  }) 
+);
+
+
   
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -38,27 +40,16 @@ app.use(passport.session());
 
 
 
-  app.use(function(req, res, next) {
+app.use(function(req, res, next) {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
     next();
   });  
 
-app.use('/', require('./routers/index'))
+app.use('/', require('./routers/auth'))
 app.use('/users', require('./routers/users'))
-
-app.get('/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: 'users/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    console.log('Redirect Successful')
-    res.redirect('users/dashboard');
-  });
-
+app.use(require('./routers/auth'))
 
 
 
