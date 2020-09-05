7fef86abcc86e9d1bac630d605cb370c311139aa
Raj Anand <58350132+Just-A-Pixel@users.noreply.github.com>
2020-09-05 23:21:28 +0530

    Update login-script.js

--- Frontend/assets/js/login-script.js
+++ Frontend/assets/js/login-script.js
@@ -1,4 +1,23 @@
-// 
+var getParams = function (url) {
+	var params = {};
+	var parser = document.createElement('a');
+	parser.href = url;
+	var query = parser.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	return params;
+};
+
+
+
+
+
+
+
+
 
 function login() {
     dataEmail = document.getElementById("username").innerHTML
@@ -8,13 +27,13 @@ if(dataEmail == "" || dataPass == ""){
     // break;
 }
     
-$.post("https://codechefvitbugs.herokuapp.com/users/login", 
-    {
-        email: dataEmail,
-        password: dataPass
-    },
-    function(data,status){
-        console.log(data)
-        console.log(status)
-        })
+    // $.get("http://codechefvitbugs.herokuapp.com/auth/google", 
+    //     // {
+    //     //     email: dataEmail,
+    //     //     password: dataPass
+    //     // },
+    //     function(data,status){
+    //         console.log(data)
+    //         console.log(status)
+    //         })
 }
