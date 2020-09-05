188b47d385606a00e85939d4a30d3b026dea2bb4
Raj Anand <58350132+Just-A-Pixel@users.noreply.github.com>
2020-09-05 23:20:53 +0530

    Update script.js

--- Frontend/assets/js/script.js
+++ Frontend/assets/js/script.js
@@ -1,15 +1,51 @@
+// "https://codechefvitbugs.herokuapp.com/users/getallusers"
+
+// $.get("https://codechefvitbugs.herokuapp.com/users/getallusers". function(data) {
+    
+// })
+console.log(window.location.href)
+
+//var string = "http://127.0.0.1:5500/CODECHEF%20bug%20site/bugs-all.html/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjBkZGQ4YjhmYzc5NTAwMTc1NGI4NjEiLCJuYW1lIjoiUkFKIEFOQU5EIDE5QkNFMDE2NSIsImVtYWlsIjoicmFqLmFuYW5kMjAxOUB2aXRzdHVkZW50LmFjLmluIiwiaWF0IjoxNTk5MzI3NjQ3LCJleHAiOjE1OTk0MTQwNDd9.4nFGcP2d3MULvKr5vdVJuMEhZOAOnsgHgOj7aEbXj5c&_id=5f0ddd8b8fc795001754b861"
+var string = window.location.href
+string = string.split("?")
+string = string[1].split("&")
+string = string[0].split("=")
+token = string[1]
+console.log(token)
+
+
+
+var test_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjBkZGQ4YjhmYzc5NTAwMTc1NGI4NjEiLCJuYW1lIjoiUkFKIEFOQU5EIDE5QkNFMDE2NSIsImVtYWlsIjoicmFqLmFuYW5kMjAxOUB2aXRzdHVkZW50LmFjLmluIiwiaWF0IjoxNTk5MzE0NDY5LCJleHAiOjE1OTk0MDA4Njl9.pCuE6swzF68vgu0d-s2oeQ_TBf8ac8WFXxaOwsNZx2E'
+
+$.ajax({
+    url: 'https://codechefvitbugs.herokuapp.com/report/bug/all',
+    type: 'GET',
+    beforeSend: function (xhr) {
+        xhr.setRequestHeader('auth-token', test_token);
+    },
+    success: function (data, status) { for(i=0; i<data.length; i++) {
+                document.getElementById("test").innerHTML+= '<a href="#issue-list"><li class="project-element"  onclick="select(this)" onmouseover="addAnimate(this)" onmouseout="removeAnimate(this)">'+data[i].project+'</li></a><br>'
+                document.getElementById("project").innerHTML += "<option>"+data[i].project+"</option>"
+            }
+            console.log("Recieved projects list and size = "+i)},
+    error: function () { },
+    });
+
+
+
 
 
 var currentObject = 0;
 
-$.get('https://codechefvitbugs.herokuapp.com/report/bug/all', function(data, status){
+// $.get('https://codechefvitbugs.herokuapp.com/report/bug/all', function(data, status){
+
   
-    for(i=0; i<data.length; i++) {
-        document.getElementById("test").innerHTML+= '<li class="project-element"  onclick="select(this)" onmouseover="addAnimate(this)" onmouseout="removeAnimate(this)">'+data[i].project+'</li><br>'
-        document.getElementById("project").innerHTML += "<option>"+data[i].project+"</option>"
-    }
-    console.log("Recieved projects list and size = "+i)
-});
+//     for(i=0; i<data.length; i++) {
+//         document.getElementById("test").innerHTML+= '<a href="#issue-list"><li class="project-element"  onclick="select(this)" onmouseover="addAnimate(this)" onmouseout="removeAnimate(this)">'+data[i].project+'</li></a><br>'
+//         document.getElementById("project").innerHTML += "<option>"+data[i].project+"</option>"
+//     }
+//     console.log("Recieved projects list and size = "+i)
+// });
 
 // Side project list animations
 
@@ -27,48 +63,53 @@ function select(x) {
     x.classList.remove("project-element")
 
     document.getElementById("issue-list").innerHTML = ''
-    $.get('https://codechefvitbugs.herokuapp.com/report/bug/all', function(data, status){
 
-        for(i=0; i<data.length; i++) {
-            if(data[i].project == x.innerHTML) {
-                console.log("success")
-                for(j=0; j<data[i].alpha.length; j++){
-                console.log(data[i].project)
-                
-                var commentString = ""
-                for(k=0; k<data[i].alpha[j].commentsByUsers.length; k++){
-                                                    commentString +=  
-                                                                    '<p class="container comment-title">'+
-                                                                    '<img src="assets/css/img/Ellipse 11.svg" class="bug2">'+
-                                                                    '<span class = "comment-name">'+data[i].alpha[j].commentsByUsers[k].name+'</span><br>'+
-                                                                    '<span class = "comment">'+data[i].alpha[j].commentsByUsers[k].discussions+'</span>'+
-                                                                    '</p>'+'<hr>'}
-
-                document.getElementById("issue-list").innerHTML += '<div class="container tweet">'+
-                                                                        '<p class="title">'+
-                                                                            '<img src="assets/css/img/bug.svg" class="bug">'+
-                                                                            //'<div class="btn remove-tweet" onclick="removeBug(this)">X</div>'+
-                                                                            '<span class = "tweet-name">'+data[i].alpha[j].issuedby+'</span><br>'+
-                                                                            '<span class = "tweet-project">Found a bug in: '+data[i].project+'</span>'+
-                                                                        '</p>'+
-                                                                        '<hr>'+
-                                                                    '<h4 class="tweet-title">'+data[i].alpha[j].title+'</h4>'+
-                                                                    '<p class="tweet-description">'+data[i].alpha[j].description+'</p>'+
-                                                                    '<img src="assets/css/img/chat.svg" style="width: 20px;"> <span class="show-comment-button" onclick="showComment(this)">Comments</span>'+
-                                                                        '<div class="comment-box " style="display: none;">'+
-                                                                            '<div class=" container text-center input" ><input type="text" class = "add-comment" > <button class="btn btn-primary submit-comment" onclick="addComment(this)">Comment</button>'+
-                                                                            '</div>'+
-                                                                            '<hr>'+ commentString +
-                                                                        '</div>'+
-                                                                        '<h6 class = "tweet-id "onselectstart="return false">'+data[i].alpha[j]._id+'</h6>'+
-                                                                    '</div>'
-                console.log(data[i].alpha[j].commentsByUsers)
-                }
-            }
-        }
+    $.ajax({
+        url: 'https://codechefvitbugs.herokuapp.com/report/bug/all',
+        type: 'GET',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('auth-token', test_token);
+        },
+        data: {},
+        success: function (data, status) { for(i=0; i<data.length; i++) {
+                if(data[i].project == x.innerHTML) {
+                    console.log("success")
+                    for(j=0; j<data[i].alpha.length; j++){
+                    console.log(data[i].project)
+                    
+                    var commentString = ""
+                    for(k=0; k<data[i].alpha[j].commentsByUsers.length; k++){
+                                                        commentString +='<hr>' +
+                                                                        '<p class="container comment-title">'+
+                                                                        '<img src="assets/css/img/Ellipse 11.svg" class="bug2">'+
+                                                                        '<span class = "comment-name">'+data[i].alpha[j].commentsByUsers[data[i].alpha[j].commentsByUsers.length-1-k].name+'</span><br>'+
+                                                                        '<span class = "comment">'+data[i].alpha[j].commentsByUsers[data[i].alpha[j].commentsByUsers.length-1-k].discussions+'</span>'+
+                                                                        '</p>'}
     
-    });
-
+                    document.getElementById("issue-list").innerHTML += '<div class="container tweet">'+
+                                                                            '<p class="title">'+
+                                                                                '<img src="assets/css/img/bug.svg" class="bug">'+
+                                                                                //'<div class="btn remove-tweet" onclick="removeBug(this)">X</div>'+
+                                                                                '<span class = "tweet-name">'+data[i].alpha[j].issuedby+'</span><br>'+
+                                                                                '<span class = "tweet-project">Found a bug in: '+data[i].project+'</span>'+
+                                                                            '</p>'+
+                                                                            '<hr>'+
+                                                                        '<h4 class="tweet-title">'+data[i].alpha[j].title+'</h4>'+
+                                                                        '<p class="tweet-description">'+data[i].alpha[j].description+'</p>'+
+                                                                        '<img src="assets/css/img/chat.svg" style="width: 20px;"> <span class="show-comment-button" onclick="showComment(this)">Comments</span>'+
+                                                                            '<div class="comment-box " style="display: none;">'+
+                                                                                '<div class=" container text-center input" ><input type="text" class = "add-comment" > <button class="btn btn-primary submit-comment" onclick="addComment(this)">Comment</button>'+
+                                                                                '</div>'+
+                                                                                 commentString +
+                                                                            '</div>'+
+                                                                            '<h6 class = "tweet-id "onselectstart="return false">'+data[i].alpha[j]._id+'</h6>'+
+                                                                        '</div>'
+                    console.log(data[i].alpha[j].commentsByUsers)
+                    }
+                }
+            }},
+        error: function () { },
+        });
 }
 
 // Add a new issue
@@ -102,17 +143,39 @@ function submitBug() {
 
     console.log()
     
-    $.post("https://codechefvitbugs.herokuapp.com/report/reportbug", 
-    {
-        project: bugProject,
-        title: bugTitle,
-        description: bugDescription,
-        issuedby: bugIssuedBy
-    },
-    function(data,status){
-        console.log(data)
-        console.log(status)
-        })
+    // $.post("https://codechefvitbugs.herokuapp.com/report/reportbug", 
+    // {
+    //     project: bugProject,
+    //     title: bugTitle,
+    //     description: bugDescription,
+    //     issuedby: bugIssuedBy
+    // },
+    // function(data,status){
+    //     console.log(data)
+    //     console.log(status)
+    //     alert("Bug Submitted")
+    //     })
+
+
+        
+    $.ajax({
+        url: 'https://codechefvitbugs.herokuapp.com/report/reportbug',
+        type: 'POST',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('auth-token', test_token);
+        },
+        data: {
+            project: bugProject,
+            title: bugTitle,
+            description: bugDescription,
+            issuedby: bugIssuedBy
+        },
+        success: function(result) {
+            alert("Bug Submitted")
+        }
+    });
+
+
     }
 }
 
@@ -128,6 +191,9 @@ function removeBug(x) {
         $.ajax({
             url: 'https://codechefvitbugs.herokuapp.com/report/deletebug/'+idToDelete,
             type: 'DELETE',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('auth-token', test_token);
+            },
             success: function(result) {
                 console.log("Deleted")
             }
@@ -147,17 +213,20 @@ function reveal(x) {
 
 function submitProject() {
     r = document.getElementById("project-input")
-    // alert(r.value)
 
-    $.post("https://codechefvitbugs.herokuapp.com/report/addprojectcodechef", 
-    {
-        project: r.value,
-    },
-    function(data,status){
-        console.log(data)
-        console.log(status)
-        })
-
+    $.ajax({
+        url: 'https://codechefvitbugs.herokuapp.com/report/addprojectcodechef',
+        type: 'POST',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('auth-token', test_token);
+        },
+        data: {
+            project: r.value,
+        },
+        success: function(data,status) {
+            alert("Project added")
+        }
+    });
 
     r.value = null
     document.getElementById("left-side").style.display = "none"
@@ -192,19 +261,33 @@ function addComment(x) {
     issueId = x.parentNode.parentNode.parentNode.lastElementChild.innerHTML
     patch = x.parentNode.firstElementChild.value
 
-    console.log(patch)
+    console.log(x.parentNode)
+    var txt =  
+                '<hr>' +
+                '<p class="container comment-title">'+
+                '<img src="assets/css/img/Ellipse 11.svg" class="bug2">'+
+                '<span class = "comment-name">'+"DJ"+'</span><br>'+
+                '<span class = "comment">'+patch+'</span>'+
+                '</p>'
+    if (patch != ""){
+        $(x.parentNode).after(txt);
+        alert("Added Comment")
 
-    $.ajax({
-        type: 'PATCH',
-        url: 'https://codechefvitbugs.herokuapp.com/report/addcommentsbyusers/'+issueId,
-        data: {
-            userComments: patch,
-            issuedby: "DJ"
-        },
-        success: function(result) {
-            console.log("added comment")
-        }
-     });
+        $.ajax({
+            type: 'PATCH',
+            url: 'https://codechefvitbugs.herokuapp.com/report/addcommentsbyusers/'+issueId,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('auth-token', test_token);
+            },
+            data: {
+                userComments: patch,
+                issuedby: "DJ"
+            },
+            success: function(result) {
+                console.log("added comment")
+            }
+         });
+    }
 }
 
 function showComment(x) {
@@ -220,3 +303,15 @@ function logout() {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+//"http://127.0.0.1:5500/CODECHEF%20bug%20site/bugs-all.html/?name=RAJ%20ANAND%2019BCE0165&   token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjBkZGQ4YjhmYzc5NTAwMTc1NGI4NjEiLCJuYW1lIjoiUkFKIEFOQU5EIDE5QkNFMDE2NSIsImVtYWlsIjoicmFqLmFuYW5kMjAxOUB2aXRzdHVkZW50LmFjLmluIiwiaWF0IjoxNTk4ODkxNzExLCJleHAiOjE1OTg5NzgxMTF9.jJtgC0Xw_Pwqbk5wOhlwaWb5-1bR9JiEhaA085MyrJM    &email=raj.anand2019@vitstudent.ac.in&googleId=104528509705348404316&_id=5f0ddd8b8fc795001754b861"
